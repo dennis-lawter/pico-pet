b387ec93e6053b96b4b3ede3670c28bc45d671e6
Bytomancer <dennis.lawter@gmail.com>
2023-09-06 00:37:34 -0400

    stripped down code

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,36 @@ mod globals;
 mod setting_value;
 mod states;
 mod system;
+use cortex_m::delay::Delay;
 
-use crate::system::SystemComponents;
+use debugless_unwrap::DebuglessUnwrap;
+use embedded_graphics::{
+    pixelcolor::Rgb565,
+    prelude::{DrawTarget, RgbColor},
+};
+use embedded_hal::{
+    digital::v2::{InputPin, OutputPin},
+    prelude::_embedded_hal_blocking_i2c_Read,
+    PwmPin,
+};
+use fugit::RateExtU32;
+
+use waveshare_rp2040_lcd_0_96::{
+    hal::{
+        self,
+        clocks::{init_clocks_and_plls, Clock},
+        gpio::Pins,
+        pac,
+        pio::PIOExt,
+        sio::SioFifo,
+        watchdog::Watchdog,
+        Sio,
+    },
+    pac::{PPB, PSM},
+    XOSC_CRYSTAL_FREQ,
+};
+
+use st7735_lcd::{Orientation, ST7735};
 
 use waveshare_rp2040_lcd_0_96::{entry, hal::multicore::Multicore};
 
@@ -31,31 +59,64 @@ use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let mut system = SystemComponents::new();
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
 
-    init_globals();
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    spawn_secondary_core_worker(&mut system);
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
 
-    cores::primary_main_loop(&mut system)
-}
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
 
-fn init_globals() {
-    display::text_writer::init_singleton_fonts();
-}
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
 
-fn spawn_secondary_core_worker(system: &mut SystemComponents) {
-    unsafe {
-        let mut mc = Multicore::new(
-            &mut *system.psm_ptr,
-            &mut *system.ppb_ptr,
-            &mut *system.fifo_ptr,
-        );
-        let cores = &mut mc.cores();
-        let core1 = &mut cores[1];
-        let sys_freq = system.sys_freq;
-        let _test = core1.spawn(&mut cores::secondary_core::CORE1_STACK.mem, move || {
-            cores::secondary_main_loop(sys_freq)
-        });
+    // Configure two pins as being I²C, not GPIO
+    let sda_pin = pins.gpio0.into_mode::<hal::gpio::FunctionI2C>();
+    let scl_pin = pins.gpio1.into_mode::<hal::gpio::FunctionI2C>();
+    // let not_an_scl_pin = pins.gpio20.into_function::<hal::gpio::FunctionI2C>();
+
+    // Create the I²C drive, using the two pre-configured pins. This will fail
+    // at compile time if the pins are in the wrong mode, or if this I²C
+    // peripheral isn't available on these pins!
+    let mut i2c = hal::I2C::i2c0(
+        pac.I2C0,
+        sda_pin,
+        scl_pin, // Try `not_an_scl_pin` here
+        400.kHz(),
+        &mut pac.RESETS,
+        &clocks.system_clock,
+    );
+
+    let sys_freq = clocks.system_clock.freq().to_Hz();
+    let mut timer = Delay::new(core.SYST, sys_freq);
+
+    let mut buffer = [0u8; 1];
+    // i2c.read(0x86, &mut buffer).unwrap();
+
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+    loop {
+        led_pin.set_high().unwrap();
+        timer.delay_ms(500);
+        led_pin.set_low().unwrap();
+        timer.delay_ms(500);
     }
 }
