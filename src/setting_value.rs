eb12dcc34fc6b781b00e2bd31608b60083af267c
Bytomancer <dennis.lawter@gmail.com>
2023-09-12 22:32:21 -0400

    Refactor

--- src/setting_value.rs
+++ src/setting_value.rs
@@ -5,16 +5,14 @@ pub struct Setting {
     pub value: u8,
     pub max_value: u8,
 }
+
 impl Setting {
     pub fn get_value(&self) -> u8 {
         self.value
     }
 
     pub fn inc(&mut self) {
-        self.value += 1;
-        if self.value > self.max_value {
-            self.value = self.max_value;
-        }
+        self.value = (self.value + 1).min(self.max_value);
     }
 
     pub fn dec(&mut self) {
@@ -24,31 +22,32 @@ impl Setting {
     pub fn generate_bar(&self, active: bool) -> &'static str {
         static mut BUFFER: [u8; SETTING_BAR_SIZE + 1] = [b'='; SETTING_BAR_SIZE + 1];
 
-        let value_out_of_15 = (((self.value as u32) * 15) / (self.max_value as u32)) as u8;
+        let value_out_of_15 = (((self.value as u32) * 15) / (self.max_value as u32)) as usize;
 
         unsafe {
-            BUFFER[0] = b'[';
-            BUFFER[SETTING_BAR_SIZE] = b']';
-            for i in 0..=SETTING_BAR_SIZE {
-                if i as u8 == value_out_of_15 && i > 0 {
-                    if active {
-                        BUFFER[i as usize] = b'5';
-                    } else {
-                        BUFFER[i as usize] = b'4';
-                    }
-                } else if (i as u8) < value_out_of_15 {
-                    BUFFER[i as usize] = b'4';
-                } else if i == SETTING_BAR_SIZE {
-                    BUFFER[i as usize] = b']';
-                } else if i == 0 {
-                    if active {
-                        BUFFER[i as usize] = b'5';
-                    } else {
-                        BUFFER[i as usize] = b'[';
-                    }
+            BUFFER[0] = if self.value == 0 {
+                if active {
+                    b'5'
                 } else {
-                    BUFFER[i as usize] = b'=';
+                    b'['
                 }
+            } else {
+                b'4'
+            };
+
+            BUFFER[SETTING_BAR_SIZE] = b']';
+            for i in 1..SETTING_BAR_SIZE {
+                BUFFER[i] = match i.cmp(&value_out_of_15) {
+                    core::cmp::Ordering::Less => b'4',
+                    core::cmp::Ordering::Equal => {
+                        if active {
+                            b'5'
+                        } else {
+                            b'4'
+                        }
+                    }
+                    _ => b'=',
+                };
             }
 
             core::str::from_utf8_unchecked(&BUFFER)
