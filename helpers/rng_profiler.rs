3b9e06bebe2ba09c7a45031ec58351691b3237ab
Bytomancer <dennis.lawter@gmail.com>
2023-09-20 23:59:47 -0400

    Rng profiling

--- helpers/rng_profiler.rs
+++ helpers/rng_profiler.rs
@@ -3,13 +3,9 @@ struct Lcg {
 }
 
 impl Lcg {
-    const M: u32 = (1 << 12);
-    const A: u32 = 69069;
-    const C: u32 = 1;
-
-    // const M: u32 = (1 << 16) + 1;
-    // const A: u32 = 75;
-    // const C: u32 = 74;
+    const M: u32 = (1 << 16) + 1;
+    const A: u32 = 75;
+    const C: u32 = 74;
 
     pub fn new(seed: u32) -> Self {
         Self { state: seed }
@@ -27,33 +23,36 @@ impl Lcg {
     }
 }
 
-fn find_period(rng: &mut Lcg) -> usize {
-    let mut tortoise = rng.next();
-    let mut hare = rng.next();
-    hare = rng.next(); // Move hare two steps ahead
+fn find_period(rng1: &mut Lcg, rng2: &mut Lcg) -> usize {
+    let mut tortoise = rng1.next();
+
+    rng2.next();
+    let mut hare = rng2.next();
 
     // Phase 1: Detect a repetition
     while tortoise != hare {
-        tortoise = rng.next();
-        hare = rng.next();
-        hare = rng.next();
+        tortoise = rng1.next();
+        rng2.next();
+        hare = rng2.next();
     }
 
-    // Phase 2: Find the length of the shortest cycle
+    // Phase 2: Find the length of the cycle
     let mut length = 1;
-    hare = rng.next();
+    hare = rng2.next();
     while tortoise != hare {
-        hare = rng.next();
+        hare = rng2.next();
         length += 1;
     }
 
     length
 }
 
+const SEED_SAMPLE_SIZE: usize = 86400;
+
 fn main() {
     let mut rng = Lcg::new(1);
     const ITERATIONS: usize = 1 << 16;
-    let mut counts = [0u32; 256]; // array to store counts of each byte
+    let mut counts = [0u32; 256];
 
     for _ in 0..ITERATIONS {
         let byte = rng.next();
@@ -66,6 +65,52 @@ fn main() {
 
     println!("================================");
 
-    let period = find_period(&mut rng);
-    println!("The period of the LCG is: {}", period);
+    let mut cycles = [0usize; SEED_SAMPLE_SIZE];
+
+    for i in 0..SEED_SAMPLE_SIZE {
+        let mut rng1 = Lcg::new(i as u32);
+        let mut rng2 = Lcg::new(i as u32);
+
+        let period = find_period(&mut rng1, &mut rng2);
+        cycles[i] = period;
+    }
+
+    let mut shortest_cycle = usize::MAX;
+    let mut shortest_seed = 0usize;
+    let mut longest_cycle = 0usize;
+    let mut longest_seed = 0usize;
+
+    for i in 0..SEED_SAMPLE_SIZE {
+        if cycles[i] < shortest_cycle {
+            shortest_seed = i;
+            shortest_cycle = cycles[i];
+        }
+        if cycles[i] > longest_cycle {
+            longest_seed = i;
+            longest_cycle = cycles[i];
+        }
+    }
+
+    let average_cycle: usize = cycles.iter().sum::<usize>() / SEED_SAMPLE_SIZE;
+
+    println!(
+        "The shortest period of the LCG is: {} @ seed {}",
+        shortest_cycle, shortest_seed
+    );
+    println!(
+        "The longest period of the LCG is:  {} @ seed {}",
+        longest_cycle, longest_seed
+    );
+    println!("The average period of the LCG is:  {}", average_cycle);
+
+    println!("================================");
+    println!("Worst seed:");
+
+    let mut worst_rng = Lcg::new(shortest_seed as u32);
+    for i in 0..256 {
+        print!("{:#04x}\t", worst_rng.next());
+        if i % 8 == 7 {
+            println!("");
+        }
+    }
 }
