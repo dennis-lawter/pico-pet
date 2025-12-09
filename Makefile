ff6f155e115ffef64b336c312fedefe6740b43b2
Bytomancer <bytomancer@gmail.com>
2025-12-08 19:43:35 -0500

    Fixes for bad flake

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ submodules:
 install-deps:
 	cd helpers/peat_track_compiler && cargo build --release
 	cargo install --path helpers/peat_track_compiler
-	cd helpers/peat_track_player && cargo build --release
-	cargo install --path helpers/peat_track_player
+	# cd helpers/peat_track_player && cargo build --release
+	# cargo install --path helpers/peat_track_player
 	cd helpers/rgba8888-to-rgb332 && cargo build --release
 	cargo install --path helpers/rgba8888-to-rgb332
 
--- flake.nix
+++ flake.nix
@@ -1,32 +1,45 @@
 {
   description = "Build environment for pico-pet";
-  inputs =
-  {
+
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
-  
-  outputs = { self, nixpkgs, ... }@inputs:
+
+  outputs = { self, nixpkgs, rust-overlay, ... }@inputs:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ rust-overlay.overlays.default ];
+    };
+
+    rust = pkgs.rust-bin.nightly."2025-12-01".default.override {
+      extensions = [
+        "rust-src"
+        "rust-std"
+      ];
+
+      targets = [
+        "thumbv6m-none-eabi"
+      ];
+    };
   in
   {
-    devShells.${system}.default = pkgs.mkShell
-    {
+    devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
-        rustc
-        cargo
+        rust
         alsa-lib
         pkg-config
         udev
         freecad
         kicad
+        elf2uf2-rs
       ];
+
       shellHook = ''
-        rustup toolchain install nightly-2023-11-16
-        rustup default nightly-2023-11-16
-        rustup target add thumbv6m-none-eabi
-        export RUSTC=$(rustup which rustc)
+        export PATH=${rust}/bin:$PATH
+        export RUST_SRC_PATH=${rust}/lib/rustlib/src/rust/library
       '';
     };
   };
