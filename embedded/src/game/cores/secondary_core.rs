8cf82f6ab9b1076ad744a3299a597f3cedd879cd
Bytomancer <bytomancer@gmail.com>
2026-01-21 19:22:57 -0500

    Added tone player on core1 to show core1 can affect the audio engine

--- embedded/src/game/cores/secondary_core.rs
+++ embedded/src/game/cores/secondary_core.rs
@@ -1,13 +1,17 @@
 use waveshare_rp2040_lcd_0_96::hal::multicore::Stack;
 use waveshare_rp2040_lcd_0_96::pac;
 
+use crate::game::hardware::audio::AudioFrequency;
+
 pub static mut CORE1_STACK: Stack<4096> = Stack::new();
 pub fn secondary_main_loop(sys_freq: u32) -> ! {
     let core = unsafe { pac::CorePeripherals::steal() };
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, sys_freq);
     loop {
-        cortex_m::asm::wfi();
+        // cortex_m::asm::wfi();
+        let hardware = crate::game::globals::get_hardware();
+        hardware.start_tone(&AudioFrequency::Cs5);
         delay.delay_ms(1000);
     }
 }
